536ef796bc6628c75dd73f6db924de0aecd50867
viaji5557 <91449477+viaji5557@users.noreply.github.com>
2021-10-14 15:43:24 +0530

    codebatjava

---
+++ src/main/java/more20.java
@@ -0,0 +1,10 @@
+public class more20 {
+    public static void main (String [] args ){
+
+    }
+    public boolean more20(int n) {
+        int a=n%20;
+        return a<3&&a!=0;
+    }
+
+}
--- src/main/java/specialEleven.java
+++ src/main/java/specialEleven.java
@@ -1,4 +1,8 @@
 public class specialEleven {
     public static void main (String [] args){
     }
+    public boolean specialEleven(int n) {
+        return n%11==0 || n%11==1;
+    }
+
 }
